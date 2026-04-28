b1aea9c140e73f44fd8eb050296674d4c8f38093
Daram Pranay <pranaydaram07@gmail.com>
2026-04-28 21:27:42 +0530

    Create receiver.ino

---
+++ code/receiver/receiver.ino
@@ -0,0 +1,37 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+#define ss 10
+#define rst 9
+#define dio0 2
+
+void setup() {
+Serial.begin(9600);
+while (!Serial);
+
+LoRa.setPins(ss, rst, dio0);
+
+if (!LoRa.begin(433E6)) {
+Serial.println("LoRa init failed!");
+while (1);
+}
+
+Serial.println("LoRa Receiver Ready");
+}
+
+void loop() {
+int packetSize = LoRa.parsePacket();
+
+if (packetSize) {
+Serial.print("Received: ");
+
+```
+while (LoRa.available()) {
+  Serial.print((char)LoRa.read());
+}
+
+Serial.println();
+```
+
+}
+}
